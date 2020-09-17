4c1410a25315c83df46fde487aff0029a7173c97
Mona Lisa <vitobernal@hotmail.com>
2020-09-17 10:21:14 -0300

    docs

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -3,13 +3,13 @@ export const getGifs = async (categorye) => {
     const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(categorye)}&limit=100&api_key=Y8lfMQ7O1zPm2K89wQJehUlkM0OMtONU`
     const respuesta = await fetch(url)
     const { data } = await respuesta.json()
-    const gift = data.map((img) => {
+    const gift = data.map((imgs) => {
 
         return {
 
-            id: img.id,
-            title: img.title,
-            url: img.images.downsized_medium.url,
+            id: imgs.id,
+            title: imgs.title,
+            url: imgs.images.downsized_medium.url,
             img : [1,2,3,4]
         }
     })
