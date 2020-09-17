862237fa6faf84e723922491c418e0281565b757
Vito Bernal <vitobernal@hotmail.com>
2020-09-17 09:51:52 -0300

    html actualizado17

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,6 +1,6 @@
 export const getGifs = async (categorye) => {
 
-   
+    const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(categorye)}&limit=100&api_key=Y8lfMQ7O1zPm2K89wQJehUlkM0OMtONU`
     const respuesta = await fetch(url)
     const { data } = await respuesta.json()
     const gift = data.map((img) => {
